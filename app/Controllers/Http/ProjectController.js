7ff922b840a2543e1d7c3a784ca29b2b2585b918
Edwin Chen <mredwinchen@foxmail.com>
2019-04-30 16:29:48 +0800

    destroy whit exceptions

--- app/Controllers/Http/ProjectController.js
+++ app/Controllers/Http/ProjectController.js
@@ -1,21 +1,31 @@
 'use strict'
 
 const Project = use("App/Models/Project")
+const AuthorizationService = use("App/Services/AuthorizationService")
 class ProjectController {
   async index({ auth }) {
     const user = await auth.getUser();
-    console.log(user)
     return await user.projects().fetch();
   }
 
   async create({ auth, request }) {
     const user = await auth.getUser();
-    const {title} = request.all();
+    const { title } = request.all();
     const project = new Project();
     project.fill({
       title,
     });
     await user.projects().save(project);
+    return project;
+  }
+
+  async destroy({ auth, request, params }) {
+    const user = await auth.getUser();
+    const { id } = params;
+    const project = await Project.find(id);
+    console.log(project)
+    AuthorizationService.verifyPermission(project, user);
+    await project.delete();
     return project;
   }
 }
