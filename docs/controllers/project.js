fa6415935bd0675691bf1c285e8c547eb08ffc36
edwin.chen <edwin.chen@bamboonetworks.com>
2019-05-16 11:03:59 +0800

    create swagger

---
+++ docs/controllers/project.js
@@ -0,0 +1,20 @@
+/**
+ * @swagger
+ * /api/projects:
+ *   get:
+ *     tags:
+ *       - Project
+ *     summary: Sample API
+ *     description: Creates a user
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: user
+ *         description: User object
+ *         in:  body
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: users
+ */
